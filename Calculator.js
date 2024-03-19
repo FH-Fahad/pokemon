46cae04af16701a949303f4d43c5a54d9a76a0ba
Fahad Hossain <fahadbin8900@gmail.com>
2024-03-19 15:44:29 +0600

    Modified function in dev01 branch

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,11 @@
 
 const add = (a, b) => {
+    console.log('Adding', a, 'and', b);
     return a + b;
 }
 
 const subtract = (a, b) => {
+    console.log('Subtracting', b, 'from', a);
     return a - b;
 }
 
